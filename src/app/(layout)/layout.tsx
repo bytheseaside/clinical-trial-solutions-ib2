2a11fc2f9d37fc7053bc50f22f1b9e93ba92fcf3
Brisa Rojas <rojaasbrisa@gmail.com>
2024-06-17 20:56:24 -0300

    refactor and format

--- src/app/(layout)/layout.tsx
+++ src/app/(layout)/layout.tsx
@@ -1,17 +1,15 @@
 import React, { PropsWithChildren } from 'react';
+
 import { UserProvider } from '@auth0/nextjs-auth0/client';
 
 import { LayoutContextProvider } from 'shared/context';
 
-
 export default async function Layout({ children }: PropsWithChildren) {
   return (
-    <>
-      <UserProvider>
-        <LayoutContextProvider>
-          {children}
-        </LayoutContextProvider>
-      </UserProvider>
-    </>
+    <UserProvider>
+      <LayoutContextProvider>
+        {children}
+      </LayoutContextProvider>
+    </UserProvider>
   );
 }
--- src/app/(layout)/page.tsx
+++ src/app/(layout)/page.tsx
@@ -1,28 +1,13 @@
-"use client";
-import Image from "next/image";
-import { useUser } from "@auth0/nextjs-auth0/client";
+import React from 'react';
 
-export default function Home() {
-  const { user, error, isLoading } = useUser();
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
-  if (!user) return (
-    <main>
-      <a href="/api/auth/login">Login</a>
-    </main>
+export default async function Home() {
+  return (
+    <Box>
+      {/* to do */}
+      <Typography variant="h1">Home Page</Typography>
+    </Box>
   );
-
-  if (isLoading) return <div>Loading...</div>;
-
-  if (error) return <div>{error.message}</div>;
-
-  if (user) {
-    return (
-      <main>
-        <h1>{user.name}</h1>
-        <p>{user.email}</p>
-        <a href="/api/auth/logout">Logout</a>
-      </main>
-    );
-  }
-
 }
