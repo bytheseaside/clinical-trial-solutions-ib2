7b8a28875178f4e39b518928df8280a4761ff46c
Brisa Rojas <rojaasbrisa@gmail.com>
2024-06-22 21:30:53 -0300

    base shared footer

--- src/shared/layout/Footer/index.tsx
+++ src/shared/layout/Footer/index.tsx
@@ -4,44 +4,55 @@ import Box from '@mui/material/Box';
 import { SxProps, Theme } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
 
+import BrandLogo from '../BrandLogo';
+import Container from '../Container';
+
 type Props = {
   sx?: SxProps<Theme>;
 };
 
+// Helper function to convert hex to RGB
+const hexToRgb = (hex: string): [number, number, number] => {
+  const r = parseInt(hex.slice(1, 3), 16);
+  const g = parseInt(hex.slice(3, 5), 16);
+  const b = parseInt(hex.slice(5, 7), 16);
+  return [r, g, b];
+};
+
+// Function to generate RGBA values from a theme color and an opacity
+const getRGBA = (hexColor: string, opacity: number): string => {
+  const [r, g, b] = hexToRgb(hexColor);
+  return `rgba(${r}, ${g}, ${b}, ${opacity})`;
+};
+
 const Footer: React.FC<Props> = ({ sx }) => (
   <Box
-    component="footer"
-    sx={[
-      {
-        backgroundColor: 'secondary.main',
-        padding: '1rem',
-        display: 'flex',
-        justifyContent: 'flex-start',
-        alignItems: 'center',
-      },
-      ...(Array.isArray(sx) ? sx : [sx]),
-    ]}
+    sx={(theme) => ({
+      borderTop: '1px solid',
+      borderColor: theme.palette.secondary.dark,
+      '-webkit-box-shadow': `${getRGBA(theme.palette.secondary.main, 0.4)} 0px -5px, ${getRGBA(theme.palette.secondary.main, 0.3)} 0px -10px, ${getRGBA(theme.palette.secondary.main, 0.2)} 0px -15px, ${getRGBA(theme.palette.secondary.main, 0.1)} 0px -20px, ${getRGBA(theme.palette.secondary.main, 0.05)} 0px -25px`,
+      '-moz-box-shadow': `${getRGBA(theme.palette.secondary.main, 0.4)} 0px -5px, ${getRGBA(theme.palette.secondary.main, 0.3)} 0px -10px, ${getRGBA(theme.palette.secondary.main, 0.2)} 0px -15px, ${getRGBA(theme.palette.secondary.main, 0.1)} 0px -20px, ${getRGBA(theme.palette.secondary.main, 0.05)} 0px -25px`,
+      boxShadow: `${getRGBA(theme.palette.secondary.main, 0.4)} 0px -5px, ${getRGBA(theme.palette.secondary.main, 0.3)} 0px -10px, ${getRGBA(theme.palette.secondary.main, 0.2)} 0px -15px, ${getRGBA(theme.palette.secondary.main, 0.1)} 0px -20px, ${getRGBA(theme.palette.secondary.main, 0.05)} 0px -25px`,
+    })}
   >
-    <Box
-      sx={{
-        width: '10%',
-        minWidth: '80px',
-        maxWidth: '120px',
-        height: '100%',
-      }}
+    <Container
+      component="footer"
+      sx={[
+        {
+          py: 3,
+          color: 'tertiary.main',
+          display: 'flex',
+          gap: 3,
+          alignItems: 'center',
+        },
+        ...(Array.isArray(sx) ? sx : [sx]),
+      ]}
     >
-      <Box
-        sx={{
-          width: 50,
-          height: 50,
-          borderRadius: '50%',
-          backgroundColor: 'secondary.main',
-        }}
-      />
-    </Box>
-    <Typography variant="lead">
-      Clinical Trial Solutions
-    </Typography>
+      <BrandLogo />
+      <Typography variant="caption" display="block" color="tertiary.main">
+        Clinical Trial Solutions
+      </Typography>
+    </Container>
   </Box>
 );
 
