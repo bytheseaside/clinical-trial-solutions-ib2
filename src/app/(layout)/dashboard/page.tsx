d00fb31419d9cb5bdde321b0fa741b5460df82f0
Brisa Rojas <rojaasbrisa@gmail.com>
2024-07-04 11:02:56 -0300

    better naming of page

--- src/app/(layout)/dashboard/page.tsx
+++ src/app/(layout)/dashboard/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default async function PatientDashboard() {
+export default async function Dashboard() {
   return (
     <>
       General dashboard, then we have to redirect people to the correct dashboard
