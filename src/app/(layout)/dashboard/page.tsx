fdeac44755de135e4f90ec4b746b97625c74568b
Brisa Rojas <rojaasbrisa@gmail.com>
2024-07-05 12:53:37 -0300

    try now

--- src/app/(layout)/dashboard/page.tsx
+++ src/app/(layout)/dashboard/page.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 import { withPageAuthRequired } from '@auth0/nextjs-auth0/client';
 
-async function Dashboard() {
+function Dashboard() {
   return (
     <>
       General dashboard, then we have to redirect people to the correct dashboard
