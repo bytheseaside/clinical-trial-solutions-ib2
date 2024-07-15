5eb3fa236d1b5a95af2782e79dc9a61e4052d2ec
Brisa Rojas <rojaasbrisa@gmail.com>
2024-07-15 10:37:01 -0300

    adds error in case of patient not suitable

---
+++ src/app/(layout)/dashboard/not-suitable-candidate/page.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import React from 'react';
+
+import { withPageAuthRequired } from '@auth0/nextjs-auth0/client';
+import { useRouter } from 'next/navigation';
+
+import Container from 'shared/layout/Container';
+
+import Header from '../Header';
+
+function NotSuitableCandidate() {
+  const router = useRouter();
+
+  React.useEffect(() => {
+    const timeout = setTimeout(() => {
+      router.push('/api/auth/logout');
+    }, 5000);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <Container>
+        Thanks for wanting to be part of our study.
+        We are sorry to inform you that you are not a suitable candidate for this study.
+        You&apos;ll soon be logged out.
+      </Container>
+    </>
+  );
+}
+
+export default withPageAuthRequired(NotSuitableCandidate);
