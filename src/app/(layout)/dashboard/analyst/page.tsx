9e65011c2e142bb8c40de2816193a720837dbf73
Brisa Rojas <rojaasbrisa@gmail.com>
2024-07-20 10:51:05 -0300

    get of data from patients ok

--- src/app/(layout)/dashboard/analyst/page.tsx
+++ src/app/(layout)/dashboard/analyst/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
 import { getSession, withPageAuthRequired } from '@auth0/nextjs-auth0';
+import ClinicalTrialService from 'services/firebase/clinicalTrialService';
 import PatientService from 'services/firebase/patientService';
 import UserService from 'services/firebase/userService';
 
-import { Analyst, Patient } from 'shared/api';
+import { Analyst, ClinicalTrial, Patient } from 'shared/api';
 import WrongPage from 'shared/components/WrongPage';
 import { generateSymptomData } from 'shared/utils/generateSymptomData';
 
@@ -12,6 +13,27 @@ import AnalystHead from './AnalystHead';
 import AssesmentsResultsReport, { AssessmentData } from './AssesmentsResultsReport';
 import SecondaryEffectsReport from './SecondaryEffecstReport';
 
+const colors = ([
+  '#98df8a', // verde claro
+  '#c5b0d5', // morado claro
+  '#17becf', // celeste
+  '#86E3C3', // rojo claro
+  '#ff7f0e', // naranja
+  '#D0E6A5', // verde
+  '#d62728', // rojo
+  '#9467bd', // morado
+  '#8c564b', // marron
+  '#e377c2', // rosa
+  '#7f7f7f', // gris
+  '#bcbd22', // amarillo
+  '#aec7e8', // azul claro
+  '#ffbb78', // naranja claro
+  '#c49c94', // marron claro
+  '#f7b6d2', // rosa claro
+  '#c7c7c7', // gris claro
+  '#dbdb8d', // amarillo claro
+  '#9edae5', // celeste claro
+]);
 async function AnalystDashboard() {
   const session = await getSession();
   const userId = session?.user.sub;
@@ -30,131 +52,36 @@ async function AnalystDashboard() {
 
   const effectsData = generateSymptomData(patientsInClinicalTrial);
 
-  const colors = ([
-    '#98df8a', // verde claro
-    '#c5b0d5', // morado claro
-    '#17becf', // celeste
+  // eslint-disable-next-line max-len
+  const clinicalTrial: ClinicalTrial = await ClinicalTrialService.getClinicalTrialById(user.trialId);
+  const { studies } = clinicalTrial;
 
-    '#86E3C3', // rojo claro
-    '#ff7f0e', // naranja
-    '#D0E6A5', // verde
-    '#d62728', // rojo
-    '#9467bd', // morado
-    '#8c564b', // marron
-    '#e377c2', // rosa
-    '#7f7f7f', // gris
-    '#bcbd22', // amarillo
-    '#aec7e8', // azul claro
-    '#ffbb78', // naranja claro
-    '#c49c94', // marron claro
-    '#f7b6d2', // rosa claro
-    '#c7c7c7', // gris claro
-    '#dbdb8d', // amarillo claro
-    '#9edae5', // celeste claro
-  ]);
+  const assessmentData = studies.flatMap((study) =>
+    study.keyVariables.map((keyVariable) => {
+      // Ensure type is one of the allowed values
+      const type: 'boolean' | 'threshold' | 'numeric' | 'text' = keyVariable.type as 'boolean' | 'threshold' | 'numeric' | 'text';
 
-  const mockAssesmentsData: AssessmentData[] = [
-    {
-      type: 'threshold',
-      title: 'Assessment 1',
-      data: [
-        { group: 'Group A', value: 10 },
-        { group: 'Group A', value: 'NC' }, // Asegurando que 'NC' sea del tipo 'NC'
-        { group: 'Group A', value: 15 },
-        { group: 'Group B', value: 20 },
-        { group: 'Group B', value: 'NC' },
-        { group: 'Group B', value: 18 },
-        { group: 'Group C', value: 5 },
-        { group: 'Group C', value: 7 },
-      ],
-    },
-    {
-      type: 'threshold',
-      title: 'Assessment 2',
-      data: [
-        { group: 'Group A', value: 8 },
-        { group: 'Group A', value: 12 },
-        { group: 'Group B', value: 'NC' },
-        { group: 'Group B', value: 25 },
-        { group: 'Group C', value: 10 },
-        { group: 'Group C', value: 'NC' },
-        { group: 'Group C', value: 14 },
-      ],
-    },
-    {
-      type: 'threshold',
-      title: 'Assessment 3',
-      data: [
-        { group: 'Group A', value: 'NC' },
-        { group: 'Group A', value: 11 },
-        { group: 'Group B', value: 22 },
-        { group: 'Group B', value: 'NC' },
-        { group: 'Group C', value: 9 },
-        { group: 'Group C', value: 'NC' },
-        { group: 'Group C', value: 13 },
-      ],
-    },
-  ];
-  const mockBooleanAssessmentsData: AssessmentData[] = [
-    {
-      type: 'boolean',
-      title: 'Assessment 4',
-      data: [
-        { group: 'Group A', value: true },
-        { group: 'Group A', value: true },
-        { group: 'Group A', value: 'NC' },
-        { group: 'Group B', value: true },
-        { group: 'Group B', value: 'NC' },
-        { group: 'Group B', value: false },
-        { group: 'Group C', value: false },
-        { group: 'Group C', value: false },
-      ],
-    },
-    {
-      type: 'boolean',
-      title: 'Assessment 5',
-      data: [
-        { group: 'Group A', value: false },
-        { group: 'Group A', value: true },
-        { group: 'Group B', value: 'NC' },
-        { group: 'Group B', value: true },
-        { group: 'Group C', value: false },
-        { group: 'Group C', value: 'NC' },
-        { group: 'Group C', value: true },
-      ],
-    },
-    {
-      type: 'boolean',
-      title: 'Assessment 6',
-      data: [
-        { group: 'Group A', value: 'NC' },
-        { group: 'Group A', value: true },
-        { group: 'Group B', value: false },
-        { group: 'Group B', value: 'NC' },
-        { group: 'Group C', value: true },
-        { group: 'Group C', value: false },
-        { group: 'Group C', value: 'NC' },
-      ],
-    },
-  ];
+      return {
+        type,
+        title: `${study.name} - ${keyVariable.name}`,
+        data: patientsInClinicalTrial.map((patient) => {
+          const patientAssessment = patient.assessments?.find((assessment) =>
+            // Ensure assessment is an object before checking properties
+            assessment && typeof keyVariable.name === 'string' && Object.prototype.hasOwnProperty.call(assessment, keyVariable.name));
 
-  const mockNumericAssessmentsData: AssessmentData[] = [
-    {
-      type: 'numeric',
-      data: [
-        { group: 'Group A', value: 10 },
-        { group: 'Group A', value: 15 },
-        { group: 'Group A', value: 20 },
-        { group: 'Group B', value: 1 },
-        { group: 'Group B', value: 1 },
-        { group: 'Group B', value: 1 },
-        { group: 'Group C', value: 40 },
-        { group: 'Group C', value: 'NC' },
-        { group: 'Group C', value: 'NC' },
-      ],
-      title: 'Numeric Data Chart',
-    },
-  ];
+          return {
+            group: patient.group || 'General',
+            value: patientAssessment ? patientAssessment[keyVariable.name] : 'NC',
+          };
+        }),
+      };
+    }));
+
+  console.log('assessmentData', assessmentData);
+  // log the data inside the assesmentdata for each element to see if it is correct
+  assessmentData.forEach((data) => {
+    console.log('data', data);
+  });
 
   return (
     <>
@@ -164,9 +91,7 @@ async function AnalystDashboard() {
         colors={colors}
       />
       <AssesmentsResultsReport
-        assesmentsData={
-          [...mockNumericAssessmentsData, ...mockAssesmentsData, ...mockBooleanAssessmentsData]
-        }
+        assesmentsData={assessmentData as AssessmentData[]}
         colors={colors}
       />
     </>
