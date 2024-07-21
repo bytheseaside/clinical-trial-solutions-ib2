40c177630dacf400a3735bd97282d3d3f7e476d5
Brisa Rojas <rojaasbrisa@gmail.com>
2024-07-21 13:13:13 -0300

    export to excel okay

--- src/app/(layout)/dashboard/analyst/ExportToExcel.tsx
+++ src/app/(layout)/dashboard/analyst/ExportToExcel.tsx
@@ -27,60 +27,73 @@ const formatDate = (date: Date | string) => {
   return '';
 };
 
-const ExportToExcel: React.FC<Props> = ({ patientsData, trial, sx = [] }) => {
-  const exportToExcel = () => {
-    const knownSideEffects = trial.knownPossibleSecondaryEffects || [];
-    const studies = trial.studies || [];
+const createDataForExcel = (patientsData: Patient[], trial: ClinicalTrial) => {
+  const knownSideEffects = trial.knownPossibleSecondaryEffects || [];
+  const studies = trial.studies || [];
 
-    // Create headers
-    const headers = [
-      'Index', 'Group', 'Sex', 'Date of Birth',
-      ...knownSideEffects,
-      'Other Symptoms',
-      ...studies.flatMap((study) =>
-        study.keyVariables.map((keyVar) => `${study.name} - ${keyVar.name}`)),
-      'Observations',
-    ];
+  // Create headers
+  const headers = [
+    'Index', 'Group', 'Sex', 'Date of Birth',
+    ...knownSideEffects,
+    'Other Symptoms',
+    ...studies.flatMap((study) =>
+      study.keyVariables.map((keyVar) => `${study.name} - ${keyVar.name}`)),
+    'Observations',
+  ];
 
-    // Create data rows
-    const dataForExcel = patientsData.map((patient, index) => {
-      // Initialize the symptoms count for each known side effect
-      const symptomsCount = knownSideEffects.reduce((acc, sideEffect) => {
-        acc[sideEffect] = 0;
-        return acc;
-      }, {} as Record<string, number>);
+  // Create data rows
+  const dataForExcel = patientsData.map((patient, index) => {
+    // Initialize the symptoms count for each known side effect
+    const symptomsCount = knownSideEffects.reduce((acc, sideEffect) => {
+      acc[sideEffect] = 0;
+      return acc;
+    }, {} as Record<string, number>);
 
-      // Count the occurrence of each symptom
-      let otherSymptomsCount = 0;
-      patient.symptoms?.forEach((symptom) => {
-        if (knownSideEffects.includes(symptom.tag)) {
-          symptomsCount[symptom.tag] += 1;
-        } else {
-          otherSymptomsCount += 1;
-        }
-      });
-
-      // Collect assessments data for each study key variable
-      const assessmentsData = studies.flatMap((study) =>
-        study.keyVariables.map((keyVar) => {
-          const assessment = patient.assessments?.find(
-            (assess) => assess.keyVariableName === keyVar.name,
-          );
-          return assessment ? assessment.value : '';
-        }));
-
-      return {
-        Index: index + 1,
-        Group: patient.group || 'General',
-        Sex: patient.sex,
-        'Date of Birth': formatDate(patient.birthDate),
-        ...Object.values(symptomsCount),
-        'Other Symptoms': otherSymptomsCount,
-        ...assessmentsData,
-        Observations: patient.observations?.map((obs) => `${obs.date}: ${obs.text}`).join('; ') || '',
-      };
+    // Count the occurrence of each symptom
+    let otherSymptomsCount = 0;
+    patient.symptoms?.forEach((symptom) => {
+      if (knownSideEffects.includes(symptom.tag)) {
+        symptomsCount[symptom.tag] += 1;
+      } else {
+        otherSymptomsCount += 1;
+      }
     });
 
+    // Search for all the trial studies and their key variables, and search if
+    // the patient has any assessment for that study
+    const assessmentsData = studies.reduce((acc, study) => {
+      study.keyVariables.forEach((keyVar) => {
+        const key = keyVar.name;
+
+        // Collect all values for the current key variable across all assessments
+        const values = patient.assessments?.map((assess) => (assess[key] !== undefined ? `${assess[key]}` : 'NC')) || ['NC'];
+
+        // Find the first non-NC value if any, otherwise use 'NC'
+        const value = values.find((val) => val !== 'NC') || 'NC';
+        acc[`${study.name} - ${keyVar.name}`] = value;
+      });
+      return acc;
+    }, {} as Record<string, string>);
+
+    return {
+      Index: index + 1,
+      Group: patient.group || 'General',
+      Sex: patient.sex,
+      'Date of Birth': formatDate(patient.birthDate),
+      ...symptomsCount,
+      'Other Symptoms': otherSymptomsCount,
+      ...assessmentsData,
+      Observations: patient.observations?.map((obs) => `${obs.date}: ${obs.text}`).join('; ') || '',
+    };
+  });
+
+  return { headers, dataForExcel };
+};
+
+const ExportToExcel: React.FC<Props> = ({ patientsData, trial, sx = [] }) => {
+  const { headers, dataForExcel } = createDataForExcel(patientsData, trial);
+
+  const handleExportToExcel = () => {
     // Create worksheet
     const worksheet = XLSX.utils.json_to_sheet(dataForExcel);
     const workbook = XLSX.utils.book_new();
@@ -112,7 +125,7 @@ const ExportToExcel: React.FC<Props> = ({ patientsData, trial, sx = [] }) => {
       </Typography>
       <Button
         sx={{ backgroundColor: 'transparent', border: '1px solid', borderColor: 'primary.main', color: 'primary.main' }}
-        onClick={exportToExcel}
+        onClick={handleExportToExcel}
       >
         Export to Excel
       </Button>
